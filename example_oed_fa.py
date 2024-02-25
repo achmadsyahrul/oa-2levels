a34a374bf2efd364e68bc5a3936affa6dd81f1d1
Achmad Syahrul Hanafi <achmadsyahrul@outlook.co.id>
2024-02-25 23:52:14 +0700

    update example oed fa
    fix enumerate combination, add best min and best max with cmp oed and fa

--- example_oed_fa.py
+++ example_oed_fa.py
@@ -4,8 +4,9 @@ from oa.oa_2levels import construct_OA
 def sphere_function(x):
     return sum(x_i ** 2 for x_i in x)
 
-def enumerate_combination(row, Pi, Pn):
+def enumerate_combination(row):
     combination = np.zeros_like(Pi)
+    row = row[:len(combination)]
     for i, value in enumerate(row):
         if value == 1:
             combination[i] = Pi[i]
@@ -14,7 +15,7 @@ def enumerate_combination(row, Pi, Pn):
     return combination
 
 
-def evaluate_OED(OA, Pi, Pn, func):
+def evaluate_oed(func):
     min_val = float('inf')
     max_val = float('-inf')
     min_combination = None
@@ -24,7 +25,7 @@ def evaluate_OED(OA, Pi, Pn, func):
     # Loop through each row of OA
     for row in OA:
         # Initialize combination array
-        combination = enumerate_combination(row, Pi, Pn)
+        combination = enumerate_combination(row)
 
         # Calculate the value of the combination
         val = func(combination)
@@ -42,9 +43,9 @@ def evaluate_OED(OA, Pi, Pn, func):
 
         # Append combination and value to history
         history.append((combination, val))
-    return min_combination, min_val, max_combination, max_val, history, values
+    return min_val, max_val, min_combination, max_combination, history, values
 
-def evaluate_FA(D, values, func):
+def evaluate_fa(values, func):
     factor_analysis = np.zeros([2,D])
     fa_min_combination = [0] * D 
     fa_max_combination = [0] * D
@@ -66,9 +67,14 @@ def evaluate_FA(D, values, func):
             else:
                 fa_min_combination[j] = 2
                 fa_max_combination[j] = 1
-    
-    fa_min_value = func(enumerate_combination(np.array(fa_min_combination), Pi, Pn))
-    fa_max_value = func(enumerate_combination(np.array(fa_max_combination), Pi, Pn))
+    if np.array_equal(min_combination, enumerate_combination(np.array(fa_min_combination))):
+        fa_min_value = min_val
+    else:
+        fa_min_value = func(enumerate_combination(np.array(fa_min_combination)))
+    if np.array_equal(max_combination, enumerate_combination(np.array(fa_max_combination))):
+        fa_max_value = max_val
+    else:
+        fa_max_value = func(enumerate_combination(np.array(fa_max_combination)))
     return fa_min_value, fa_max_value, fa_min_combination, fa_max_combination
 
 # Example usage:
@@ -77,17 +83,22 @@ Pi = np.array([0, 2, 5])  # Level 1 combinations
 Pn = np.array([5, 0, 1])  # Level 2 combinations
 
 OA = construct_OA(D)
-min_combination, min_val, max_combination, max_val, history, values = evaluate_OED(OA, Pi, Pn, sphere_function)
+min_val, max_val, min_combination, max_combination, history, values = evaluate_oed(sphere_function)
 
-fa_min_value, fa_max_value, fa_min_combination, fa_max_combination = evaluate_FA(D, values, sphere_function)
+fa_min_value, fa_max_value, fa_min_combination, fa_max_combination = evaluate_fa(values, sphere_function)
+
+best_min_value = np.min([min_val, fa_min_value])
+best_max_value = np.max([max_val, fa_max_value])
 
 print("OA Best Minimum Combination:", min_combination)
 print("OA Minimum Value:", min_val)
 print("OA Best Maximum Combination:", max_combination)
 print("OA Maximum Value: {} \n".format(max_val))
 
-
 print("Best Minimum Combination Use FA:", fa_min_combination)
 print("Minimum Value:", fa_min_value)
 print("Best Maximum Combination Use FA:", fa_max_combination)
-print("Maximum Value:", fa_max_value)
+print("Maximum Value: {} \n".format(fa_max_value))
+
+print("Best Minimal Value", best_min_value)
+print("Best Maximal Value", best_max_value)
